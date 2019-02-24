e03e9e5135b537aac0cde9cb770a841ab13a408b
joeyak <joeyak97@gmail.com>
2019-02-24 13:03:04 -0500

    Add code

---
+++ JMESPathViewer/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace JMESPathViewer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ JMESPathViewer/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using DevLab.JmesPath;
+using Newtonsoft.Json;
+using System;
+using System.Windows;
+
+namespace JMESPathViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private JmesPath _jmes = new JmesPath();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            FilterTextBox.TextChanged += (o, e) => Transform();
+            InputTextBox.TextChanged += (o, e) => Transform();
+        }
+
+        private void Transform()
+        {
+            ErrorTextBlock.Text = "";
+            ErrorTextBlock.ToolTip = "";
+            if (!string.IsNullOrWhiteSpace(FilterTextBox.Text) && !string.IsNullOrWhiteSpace(InputTextBox.Text))
+            {
+                try
+                {
+                    var text = _jmes.Transform(InputTextBox.Text, FilterTextBox.Text);
+                    var obj = JsonConvert.DeserializeObject(text);
+                    if (obj is null)
+                    {
+                        ErrorTextBlock.Text = "Output not updated due to improper filter";
+                    }
+                    else
+                    {
+                        OutTextBox.Text = JsonConvert.SerializeObject(obj, Formatting.Indented);
+                    }
+                }
+                catch (Exception e)
+                {
+                    ErrorTextBlock.Text = "[ERR]" + e.ToString();
+                    ErrorTextBlock.ToolTip = e.ToString();
+                }
+            }
+        }
+    }
+}
